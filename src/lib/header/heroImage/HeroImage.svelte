ea7d60f8879b52f599ef2b2efb73367332c5954b
Marcello Lopes <marcell0lopes@outlook.com>
2022-06-17 18:57:43 -0300

    add logot o hero image

--- src/lib/header/heroImage/HeroImage.svelte
+++ src/lib/header/heroImage/HeroImage.svelte
@@ -6,7 +6,6 @@
 
 <section class="heroImage">
 	<Saos animation={'fade-in 0.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}>
-		<!-- <h1><strong>Evolution</strong> Informática</h1> -->
 		<img src={logo} alt="Evolution Informática" id="logo" />
 	</Saos>
 	<Saos animation={'fade-in 1.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) 1s both'}>
@@ -31,19 +30,8 @@
 		text-align: center;
 	}
 
-	h1 {
-		color: var(--pure-white);
-		font-weight: lighter;
-		letter-spacing: -0.2rem;
-		font-size: 4rem;
-	}
-
-	h1 strong {
-		font-weight: bold;
-	}
-
 	#logo {
-		max-width: 40%;
+		max-width: 80%;
 		margin: 2rem auto;
 	}
 
@@ -54,6 +42,11 @@
 		font-weight: lighter;
 		letter-spacing: 0.125rem;
 		font-size: 1.2rem;
+	}
+	@media (min-width: 500px) {
+		#logo {
+			max-width: 400px;
+		}
 	}
 
 	@keyframes -global-fade-in {
